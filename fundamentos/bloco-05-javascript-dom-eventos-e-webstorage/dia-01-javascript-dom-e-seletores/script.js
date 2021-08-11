8cc17c581b4c3d91bae87026adc2f152a287baa1
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-11 10:41:55 -0300

    Realizado os primeiros exercicios do dia 01 bloco 05

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.querySelector('#paragraph')
+paragraph.style.color = 'red'
+
+let movieName = (document.querySelector('#page-title').innerText = 'John Wick')
+let movieSubtitle = (document.querySelector('#subtitle').innerText =
+  'Descrição do filme:')
+let movieLore = (document.querySelector('#second-paragraph').innerText =
+  'Um assassino aposentado que é forçado a retomar suas atividades após a morte de sua esposa!')
+
+let changeParagraphColor = (document.querySelectorAll('.text')[0].style.color =
+  'blue')
+let changeColor = (document.querySelectorAll('p')[1].style.color = 'green')
