6ee8e77b0886773169ffa218d964cb77ebe185ce
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-23 19:08:01 -0300

    Exercicios bloco 06 dia 01 - 100% completos

--- fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/dia-01-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/dia-01-html-e-css-forms/script.js
@@ -47,6 +47,163 @@ function submitBtnPrevent() {
 
   function prevent(originEvent) {
     originEvent.preventDefault()
+
+    const getResumeContainer = document.querySelector('#resume-container')
+    let subtitleResumeField = document.createElement('h2')
+
+    subtitleResumeField.innerText = '>> Curriculo gerado com base em seus dados <<'
+    getResumeContainer.insertAdjacentElement('afterbegin', subtitleResumeField)
+
+    generateResume()
   }
 }
 
+function generateResume() {
+
+  getName()
+  getEmail()
+  getCPF()
+  getAdress()
+  getState()
+  getProfissionalEx()
+  previousPosition()
+  positionDescription()
+  date()
+
+  function getName() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveName = document.querySelector('#name').value
+
+    if (retrieveName.length === 0 || retrieveName.length > 40) {
+      alert('Nome invalido')
+      return
+    }
+
+    createLabel.innerText = 'Nome Completo: '
+    createListItem.innerText = retrieveName
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function getEmail() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveEmail = document.querySelector('#email').value
+
+    if (retrieveEmail.length === 0 || retrieveEmail.length > 50) {
+      alert('Email invalido')
+      return
+    }
+
+    if (retrieveEmail.includes('@') == false) {
+      alert('Email invalido')
+      return
+    }
+
+    createLabel.innerText = 'Email: '
+    createListItem.innerText = retrieveEmail
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function getCPF() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveCPF = document.querySelector('#cpf').value
+
+    createLabel.innerText = 'CPF: '
+    createListItem.innerText = retrieveCPF
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function getAdress() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveAdress = document.querySelector('#adress').value
+
+    createLabel.innerText = 'Endereço: '
+    createListItem.innerText = retrieveAdress
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function getState() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveState = document.querySelector('#states').value
+
+    createLabel.innerText = 'Estado em que reside: '
+    createListItem.innerText = retrieveState
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function getProfissionalEx() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveResume = document.querySelector('#curriculumResume').value
+
+    createLabel.innerText = 'Resumo Profissional: '
+    createListItem.innerText = retrieveResume
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function previousPosition() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrievePosition = document.querySelector('#position').value
+
+    if (retrievePosition.length === 0 || retrievePosition.length > 40) {
+      alert('Posição invalida')
+      return
+    }
+
+    createLabel.innerText = 'Ultimo cargo ocupado: '
+    createListItem.innerText = retrievePosition
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function positionDescription() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveDescription = document.querySelector('#desc-position').value
+
+    createLabel.innerText = 'Descrição do cargo: '
+    createListItem.innerText = retrieveDescription
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+
+  function date() {
+    const getResume = document.querySelector('#resume')
+    let createListItem = document.createElement('li')
+    let createLabel = document.createElement('strong')
+    const retrieveDate = document.querySelector('#date').value
+
+    createLabel.innerText = 'Data de inicio no cargo: '
+    createListItem.innerText = retrieveDate
+
+    createListItem.insertAdjacentElement('afterbegin', createLabel)
+    getResume.appendChild(createListItem)
+  }
+}
