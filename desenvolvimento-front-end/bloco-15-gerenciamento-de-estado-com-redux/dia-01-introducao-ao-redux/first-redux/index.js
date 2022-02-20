3bdca45561faf4122ad9715cf4fae515338479d7
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-02-20 19:12:04 -0300

    15.1 done

--- desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux/first-redux/index.js
+++ desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux/first-redux/index.js
@@ -1,30 +1,48 @@
 const Redux = require('redux');
 
-const INITAL_STATE = {
-  login: false,
-  email: ''
+
+const INITIAL_STATE = {
+  adidas: 0,
+  nike: 0,
+  puma: 0,
 }
 
-const fazerLogin = (email) => ({
-  type: 'LOGIN',
-  email
-})
-
-const reducer = (state = INITAL_STATE, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      return {
-        ...state,
-        login: !state.login,
-        email: action.email,
-      };
-    default:
-      return state;
+const addToProd = (numberToAdd) => {
+  return {
+    type: 'SHOES_ADD',
+    payload: {
+      numberToAdd
+    }
   }
 }
 
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SHOES_ADD':
+      return {
+        ...state,
+        adidas: parseInt(action.payload.numberToAdd + Math.random()),
+        nike: action.payload.numberToAdd + 5,
+        puma: action.payload.numberToAdd + 7,
+      }
+  
+    default:
+      return {
+        ...state,
+      }
+  }
+}
+
+
+// STORE
 const store = Redux.createStore(reducer);
 
-store.dispatch(fazerLogin('alguem@gmail.com'));
+const get = store.getState();
+const calc = get.adidas + 100;
 
-console.log(store.getState());
+store.subscribe(() => {
+  const counter = store.getState();
+  console.log(counter);
+});
+
+store.dispatch(addToProd(calc))
