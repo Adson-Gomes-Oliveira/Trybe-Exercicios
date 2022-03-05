92a186d5545ce0e22afa82ec13f67508227d1f70
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-03-05 17:36:38 -0300

    e-17.3-done-fix

--- desenvolvimento-front-end/bloco-16-context-api-react-hooks/dia-02-react-hooks/use-effect/src/App.js
+++ desenvolvimento-front-end/bloco-16-context-api-react-hooks/dia-02-react-hooks/use-effect/src/App.js
@@ -1,43 +1,55 @@
 import React, { useEffect, useState } from 'react';
 
+const INITIAL_SECONDS = 1;
+const SECONDS_TO_ADD = 1;
+const SECONDS_LIMIT = 10;
+
 function App() {
   const [randomNumber, setNumber] = useState(0);
-  const [timer, setTimer] = useState(0);
   const [feedback, setFeedback] = useState('');
   const [att, setAtt] = useState(0);
+  const [ second, setSeconds ] = useState(INITIAL_SECONDS);
 
   useEffect(() => {
-    async function timer() {
       setTimeout(() => {
         const number = parseInt((Math.random() * (100 - 0) + 0));
         setNumber(number);
         const verify = number % 3 === 0 || number % 5 === 0;
         verify ? setFeedback('Acertou') : setFeedback('Errou');
         att === 0 ? setAtt(1) : setAtt(0);
-      }, 6000);
-    }
-    timer();
+      }, 10000);
   }, [att]);
 
   useEffect(() => {
-    if(feedback) {
-      setTimeout(() => {
-        setFeedback('')
-      }, 4000)
+    if (second > SECONDS_LIMIT) {
+      setSeconds(INITIAL_SECONDS);
     }
-  }, [feedback])
+
+    const timer = setInterval(() => {
+      setSeconds(second + SECONDS_TO_ADD);
+    }, 1000)
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, [second]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setTimer(timer + 1);
-    }, 1000);
-    
-  }, [timer]);
+    const feedback = setInterval(() => {
+      setFeedback(true)
+    }, 4000);
+
+    return () => {
+      clearInterval(feedback);
+    }
+
+  }, [feedback])
+
 
   return (
     <>
       <span>{ randomNumber }</span><br /><br />
-      <span>{ timer }</span><br /><br />
+      <span>{ second }</span><br /><br />
       <span>{ feedback ? feedback : null }</span><br />
     </>
   );
